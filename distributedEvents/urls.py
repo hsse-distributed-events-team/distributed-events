9753077fc5e193142af51dacd9667570eed33285
home <dgekorel@gmail.com>
2022-11-29 02:39:03 +0500

    Создал скелет странички создания мероприятия, нужно БД

--- distributedEvents/urls.py
+++ distributedEvents/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
+from eventsSite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('create_event/', views.create_event, name='create_event'),
 ]
