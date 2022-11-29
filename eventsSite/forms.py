24615319301c10ebc134ef1812a167fe49c089eb
vlad <shubin.va@phystech.edu>
2022-11-29 17:29:12 +0300

    Make pre-backend

--- eventsSite/forms.py
+++ eventsSite/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
+"""
+    Форма для сбора информации мероприятия
 
+"""
 class Event(forms.Form):
     name = forms.CharField(label='Название мероприятия', required=True)
     privacy = forms.BooleanField(label='Приватное', required=True)
