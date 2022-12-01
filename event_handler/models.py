cee1a26f1168ff8d7d646d8121d119c2ee29449b
Rattysed <ckvortcov71@gmail.com>
2022-12-01 12:26:17 +0300

    Enum fix

--- event_handler/models.py
+++ event_handler/models.py
@@ -3,38 +3,22 @@ from user_handler.models import User
 from enum import Enum
 
 
-class StageStatus(Enum):
-    WAITING = 0
-    ACTIVE = 1
-    ENDED = 2
-
-
-class ApplicationStatus(Enum):
-    AWAITED = 0
-    ACCEPTED = 200
-    REJECTED = 400
-    BANNED = 404
-
-
-class ApplicationRoles(Enum):
-    PARTICIPANT = 0
-    STAFF = 1
-    ADMIN = 100
-
-
 class Event(models.Model):
     name = models.CharField("Название мероприятия", default="Новое мероприятие", max_length=50)
     description = models.TextField(null=True, blank=True, max_length=500)
-    users = models.ManyToManyField(User, through="Application")
-
 
 class Stage(models.Model):
+    class StageStatus(models.IntegerChoices):
+        WAITING = 0
+        ACTIVE = 1
+        ENDED = 2
+
     name = models.CharField("Название этапа", default="Новый этап", max_length=50)
     parent = models.ForeignKey(Event, on_delete=models.CASCADE)
     description = models.TextField(null=True, blank=True, max_length=500)
     preview = models.TextField(null=True, blank=True, max_length=100)
-
-    status = models.SmallIntegerField(null=True, default=StageStatus.WAITING)
+    users = models.ManyToManyField(User, through="Application")
+    status = models.SmallIntegerField(null=True, default=StageStatus.WAITING, choices=StageStatus.choices)
     time_start = models.DateTimeField(null=True, blank=True)
     time_end = models.DateTimeField(null=True, blank=True)
 
@@ -50,10 +34,21 @@ class Venue(models.Model):
 
 
 class Application(models.Model):
-    stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Status(models.IntegerChoices):
+        AWAITED = 0
+        ACCEPTED = 200
+        REJECTED = 400
+        BANNED = 404
+
+    class Roles(models.IntegerChoices):
+        PARTICIPANT = 0
+        STAFF = 1
+        ADMIN = 100
+
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     venue = models.ForeignKey(Venue, on_delete=models.SET_NULL, null=True)
-    role = models.SmallIntegerField("Роль пользователя в мероприятии", default=ApplicationRoles.PARTICIPANT)
+    role = models.SmallIntegerField("Роль", choices=Roles.choices, default=Roles.PARTICIPANT)
     last_update_time = models.DateTimeField()
-    status = models.SmallIntegerField("Код статуса заявки", default=ApplicationStatus.AWAITED)
+    status = models.SmallIntegerField("Статус заявки", default=Status.AWAITED)
     result = models.TextField("Результат события", max_length=500)  # Пусть пока что результат будет длинной строкой
