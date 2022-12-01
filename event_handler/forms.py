8484c17b007d9f9c5f21c86eda6dd4676785403f
Ivan Komarov <komarov.ie@phystech.edu>
2022-12-01 22:13:10 +0300

    Create forms.py

---
+++ event_handler/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class Event(forms.Form):
+    pass
+    # name = forms.CharField(label='Название мероприятия', required=True)
+    # privacy = forms.BooleanField(label='Приватное', required=True)
+    # date = forms.DateField(label="Сроки проведения", )
