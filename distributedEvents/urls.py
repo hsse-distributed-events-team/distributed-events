564a6b3ccdbec87145ef7acdf2f87f14b5e847dd
Dashevich <63498376+Dashevich@users.noreply.github.com>
2022-12-05 23:36:22 +0300

    update urls
    now you can go to register, profile, login, logout pages

--- distributedEvents/urls.py
+++ distributedEvents/urls.py
@@ -25,5 +25,10 @@ urlpatterns = [
     path('create_event/', views.create_event, name='create_event'),
     re_path(r'^events/event/(\d+)$', views.cur_event, name="cur_event"),
     path('', views.all_events, name="all_events"),
-    re_path(r'^event_list/page/(\d+)', views.all_events, name="all_events")
+    re_path(r'^event_list/page/(\d+)', views.all_events, name="all_events"),
+
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
