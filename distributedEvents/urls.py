80d35d1efdd6f5633b140408156ef8503c7d7c9a
vlad <shubin.va@phystech.edu>
2022-12-13 13:57:33 +0300

    Refactor project

--- distributedEvents/urls.py
+++ distributedEvents/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, re_path
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from event_handler import views
