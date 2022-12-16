1a106e84964e970b9ac464231c9f52b7e3597d72
Rattysed <ckvortcov71@gmail.com>
2022-12-16 19:48:57 +0300

    Added contacts field in Venue

--- distributedEvents/urls.py
+++ distributedEvents/urls.py
@@ -20,11 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from event_handler import views
 from user_handler import views as user_views
+from creator_handler import views as creator_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('create_event/', views.create_event, name='create_event'),
     path('all_events/<int:event_id>', views.cur_event, name="cur_event"),
     path('', views.all_events, name="all_events"),
@@ -33,4 +32,11 @@ urlpatterns = [
     path('user_profile/', user_views.profile, name='user_profile'),
     path('login/', auth_views.LoginView.as_view(template_name='user_handler/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user_handler/logout.html'), name='logout'),
+
+    # path('events/edit/<int:event_id>/venues/', creator_views., name=""),
+    # path('events/edit/<int:event_id>/venues/delete/<int:venue_id>', creator_views., name=""),
+    # path('events/edit/<int:event_id>/venues/edit/<int:venue_id>', creator_views., name=""),
+    # path('events/edit/<int:event_id>', creator_views., name=""),
+    # path('events/edit/<int:event_id>', creator_views., name=""),
+    # path('events/edit/<int:event_id>', creator_views., name=""),
 ]
