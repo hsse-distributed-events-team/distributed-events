a25f13f049273057c8879fbfee08abde67e18249
vlad <shubin.va@phystech.edu>
2022-12-17 00:07:01 +0300

    fix

--- event_handler/views.py
+++ event_handler/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.http import Http404
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from event_handler.forms import Event
