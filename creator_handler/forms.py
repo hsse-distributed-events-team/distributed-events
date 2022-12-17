21f1f47195f9c11a35fa86c3e59d8197b7675435
Ivan <komarov.ie@phystech.edu>
2022-12-17 22:41:22 +0300

    Add create_venue form and create_venue view.

---
+++ creator_handler/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class VenueForm(forms.Form):
+    """
+        Форма создания площадки
+
+    """
+
+    name = forms.CharField(label='Название', required=True, max_length=50)
+    address = forms.CharField(label='Адрес', required=True, max_length=500, widget=forms.Textarea)
+    region = forms.IntegerField(label='Регион', required=False)
+    participants_maximum = forms.IntegerField(label='Максимальное число участников', required=False)
+    contacts = forms.CharField(label='Контакты', required=False, max_length=100, widget=forms.Textarea)
