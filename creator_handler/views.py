86df1be91a247d7060ce44c97f93e0b12330e3c9
Rattysed <ckvortcov71@gmail.com>
2022-12-17 14:38:22 +0300

    fixed csrf methods

--- creator_handler/views.py
+++ creator_handler/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+import json
 
 from .db_controller import *
 
 
 @login_required(login_url="login")
 def venues_list(request, event_id: int):
+
     if not user_have_access(request.user, event_id):
         return redirect('/404')
     venues = get_venues_by_event(event_id)
@@ -19,16 +21,21 @@ def venues_list(request, event_id: int):
 
 @login_required(login_url="login")
 def delete_venue(request, event_id: int):
-    if request.method == "POST" and request.is_ajax():
-        venue_id = request.data
+    is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+    if request.method == "POST" and is_ajax:
+        print(request)
+
+        venue_id = request.POST.get("id", None)
+        print(venue_id)
         if not user_have_access(request.user, event_id):
-            return JsonResponse({"reason": "Not enough rights"}, status=400)
+            return JsonResponse({"errors": "Not enough rights"}, status=400)
         try:
             Venue.objects.get(id=venue_id).delete()
-            return JsonResponse({"reason": None}, status=200)
+            return JsonResponse({"errors": None}, status=200)
         except ObjectDoesNotExist:
-            return JsonResponse({"reason": "There is no such venue"}, status=400)
+            print("А где")
+            return JsonResponse({"errors": "There is no such venue"}, status=400)
         except Exception as e:
             print(e)
-            return JsonResponse({"reason": "Undefined server error"}, status=400)
+            return JsonResponse({"errors": "Undefined server error"}, status=400)
     return redirect('/')
