16972dfcd6e9c0fa4c2220f7ee848945d8f839f9
Dasha Lipatova <lipatova.dg@phystech.edu>
2022-12-19 18:32:02 +0300

    workable profile

--- user_handler/forms.py
+++ user_handler/forms.py
@@ -3,16 +3,16 @@ from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import User as Profile
-from user_handler.regions.regions_dict import DATA_REGIONS, DATA_CITIES
+#from user_handler.regions.regions_dict import DATA_REGIONS, DATA_CITIES
 
 
 class UserRegisterForm(UserCreationForm):  # User class
     email = forms.EmailField()
-    region = forms.ChoiceField(choices=DATA_REGIONS)
+    #region = forms.ChoiceField(choices=DATA_REGIONS)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'region']
+        fields = ['username', 'email', 'password1', 'password2']#, 'region']
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
--- user_handler/views.py
+++ user_handler/views.py
@@ -11,26 +11,25 @@ def register(request):  # place where the user can register
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            region = form.cleaned_data.get("region")
+            #region = form.cleaned_data.get("region")
             username = form.cleaned_data.get('username')
-            django_user = get_django_user(username)
-            user = create_user_for_django_user(django_user)  # НЕ ТРОГАТЬ. КОСТЫЛЬ.
+            #django_user = get_django_user(username)
+            #user = create_user_for_django_user(django_user)  # НЕ ТРОГАТЬ. КОСТЫЛЬ.
 
-            add_region_of_user(username=str(username), region=region)
+            #add_region_of_user(username=str(username), region=region)
 
             messages.success(request, f'Ваш аккаунт создан: можно войти на сайт.')
             return redirect('/login')
     else:
         form = UserRegisterForm()
 
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'user_handler/register.html', {'form': form})
 
 
 @login_required
 def profile(request):   # go to profile page
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        #print(u_form)
         if u_form.is_valid():
             u_form.save()
             messages.success(request, f'Ваш профиль успешно обновлен.')
@@ -42,7 +41,7 @@ def profile(request):   # go to profile page
         'u_form': u_form
     }
 
-    return render(request, 'user_handler/user_profile.html')
+    return render(request, 'user_handler/user_profile.html', name)
 
 
 def test(request):  # тест-функция. адрес: /test
