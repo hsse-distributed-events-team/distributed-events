abf7d50755662505d94eb77ded0196053eeaf5d2
Rattysed <ckvortcov71@gmail.com>
2022-12-19 18:04:31 +0300

    Fast venue hotfix

--- creator_handler/db_controller.py
+++ creator_handler/db_controller.py
@@ -1,4 +1,4 @@
-from event_handler.models import Event, Stage, StageStaff
+from event_handler.models import Event, Stage, StageStaff, Venue
 from user_handler.models import DjangoUser, User
 
 from django.core.exceptions import ObjectDoesNotExist
